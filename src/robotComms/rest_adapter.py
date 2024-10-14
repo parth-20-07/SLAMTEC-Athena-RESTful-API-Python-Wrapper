0424b3e4388cc28a32beec899b2c30104f29b3a2
Parth Patel <parth.pmech@gmail.com>
2024-10-14 15:53:48 -0400

    Setup GET, POST Adapter
    Missing:
       -> DELETE
       -> PUT
    -> Missing Monads for Logging

---
+++ src/robotComms/rest_adapter.py
@@ -0,0 +1,49 @@
+"""
+Module to Setup REST Protocol.
+
+Contains Standard Requests for:
+    -> GET
+    -> POST
+    -> DELETE
+    -> PUT
+
+Reference: https://www.pretzellogix.net/2021/12/08/step-2-write-a-low-level-rest-adapter/
+"""
+
+__name__ = "restAdapter"
+
+import requests
+import requests.packages
+from typing import Dict, List, Tuple
+
+
+httpStatusCode: Dict[int, str] = {
+    200: "SUCCESS",
+    204: "NO_CONTENT",
+    400: "BAD_REQUEST",
+    404: "NOT_FOUND",
+    500: "SERVER_INTERNAL_ERROR",
+}
+
+
+class restAdapter:
+    def __init__(self) -> None:
+        pass
+
+    def get(
+        self, full_endpoint: str, ep_params: Dict[str, str] = None
+    ) -> Tuple[int, List[Dict[str, str]]]:
+        response = requests.get(url=full_endpoint, params=ep_params)
+        data: List[Dict[str, str]] = response.json()
+        return (response.status_code, data)
+
+    def post(
+        self,
+        full_endpoint: str,
+        ep_params: Dict[str, str] = None,
+        data: Dict[str, str] = None,
+    ) -> int:
+        response = requests.post(url=full_end_point, params=ep_params, json=data)
+        data_out = response.json()
+
+        return (response.status_code, data_out)
