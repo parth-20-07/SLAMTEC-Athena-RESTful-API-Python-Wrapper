7ea76bf932ee4fdf07379529f2a53ecb3548b22d
Parth Patel <parth.pmech@gmail.com>
2024-11-03 18:12:35 -0500

    Complete the artifact

--- robotComms/api_classes/artifact.py
+++ robotComms/api_classes/artifact.py
@@ -139,7 +139,7 @@ class artifact:
                                 - "line" => Add Virtual Line Segment
                                 - "rect" => Add a Rectangular Area
                                 - "poi" => Add POI
-                                - "laser" =>
+                                - "laser" => Set Laser Landmarks
                             a_usage:
                                 - a_type = "lines":
                                     - "tracks" => Virtual Track
@@ -208,10 +208,9 @@ class artifact:
                                             0
                                         ]
 
-
-
                         Returns:
-
+                            - True => Add Success
+                            - False => Add Failure
         """
         self.__LOGGER.INFO(f"Request: Type:{a_type} | Usage:{a_usage} | ID:{id}")
         endpoint = ""
@@ -263,5 +262,227 @@ class artifact:
             response_type=r_type,
             body_params=dict_value,
         )
-        result: CombinedType = response.data
-        return result
+
+        status_code = response.status_code
+        if status_code == 200:
+            return True
+        else:
+            return False
+
+    def modify_artifact(
+        self,
+        a_type: str,
+        a_usage: typing.Optional[str] = None,
+        id: typing.Optional[str] = None,
+        dict_value: typing.Optional[ListDictType] = None,
+    ) -> bool | ListDictType:
+        """Modify Map Semantic Elements
+
+        Args:
+            a_type: Type of Element to Fetch
+                - "line" => Modify Virtual Line Segment
+                - "poi" => Modify POI
+                - "laser" => Setting Up Laser Landmarks
+            a_usage:
+                - a_type = "lines":
+                    - "tracks" => Virtual Track
+                    - "walls" => Virtual Wall
+                - a_type = "laser":
+                    - None => Set the laser landmark information read from the map into Slamware
+                    - "update" => Set whether to allow Slamware to automatically update laser landmarks
+            dict_value: Data to add to Map
+                Examples:
+                    - a_type = "lines" ["tracks", "walls"]: When adding, id is an invalid field and can be any value.
+                        [
+                                {
+                                    "id": 0,
+                                    "start": {
+                                        "x": 0,
+                                        "y": 0
+                                        },
+                                    "end": {
+                                        "x": 0,
+                                        "y": 0
+                                        },
+                                    "metadata": {}
+                                    }
+                        ]
+                    - a_type = "poi" [None]: The request message can contain only one of pose and metadata, and the other field remains unchanged.
+                        {
+                            "pose": {
+                                "x": 0,
+                                "y": 0,
+                                "z": 0,
+                                "yaw": 0,
+                                "picth": 0,
+                                "roll": 0
+                                },
+                            "metadata": {}
+                        }
+                    - a_type = "laser":
+                        - a_usage = None:
+                            Example:
+                                [
+                                        {
+                                            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                            "pose": {
+                                                "x": 0,
+                                                "y": 0,
+                                                "yaw": 0
+                                                },
+                                            "metadata": {}
+                                            }
+                                ]
+                        - a_usage = "update":
+                            Example:
+                                {
+                                        "enable": true
+                                }
+
+            id: ID Value for the parameter to change
+                - a_type = "poi": String UUID
+                    Example: "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+
+
+
+        Returns:
+            - True => Modify Success
+            - False => Modify Failure
+        """
+        self.__LOGGER.INFO(f"Request: Type:{a_type} | Usage:{a_usage} | ID:{id}")
+        endpoint = ""
+        # Check Input Conditions
+        if a_type == "lines":
+            if a_usage not in ["tracks", "walls"]:
+                self.__LOGGER.WARNING("Invalid Usage Argument Passed")
+                return False
+            else:
+                endpoint = f"lines/{a_usage}"
+            r_type = Response_Type.JSON
+        elif a_type == "poi":
+            a_type = "pois"
+            if id is not None:
+                endpoint = f"pois/{id}"
+                r_type = Response_Type.JSON
+            else:
+                return False
+        elif a_type == "laser":
+            if a_usage is None:
+                endpoint = "laser-landmarks"
+                r_type = Response_Type.JSON
+            elif a_usage == "update":
+                endpoint = "laser-landmarks/:update"
+                r_type = Response_Type.STR
+            else:
+                self.__LOGGER.WARNING("Invalid Usage Argument Passed")
+                return False
+            a_type = "laser-landmarks"
+        else:
+            self.__LOGGER.WARNING("Invalid Type Argument Passed")
+            return False
+
+        response: combined_Result = self.__REST_ADAPTER.put(
+            full_endpoint=f"{self.__IP_ADDR}/{self.__API_TAG}/{self.__API_VERSION}/{endpoint}",
+            response_type=r_type,
+            body_params=dict_value,
+        )
+        status_code = response.status_code
+        if status_code == 200:
+            return True
+        else:
+            return False
+
+    def delete_artifact(
+        self,
+        a_type: str,
+        a_usage: typing.Optional[str] = None,
+        id: typing.Optional[int | str] = None,
+    ) -> bool | ListDictType:
+        """Delete Map Semantic Elements
+
+        Args:
+            a_type: Type of Element to Fetch
+                - "line" => Delete Virtual Line Segment
+                - "rect" => Delete Rectangular Area
+                - "poi" => Delete POI
+                - "laser" => Delete Laser Landmarks
+            a_usage:
+                - a_type = "lines":
+                    - "tracks" => Virtual Track
+                    - "walls" => Virtual Wall
+                - a_type = "rect":
+                    - "forbidden_area"
+                    - "elevator_area"
+                    - "dangerous_area"
+                    - "coverage_area"
+                    - "maintenance_area"
+                    - "sensor_disable_area"
+                    - "restricted_area"
+                - a_type = "laser":
+                    - None => Clear all laser landmarks
+            id: ID Value for the parameter to change
+                - a_type = "lines": ["tracks", "walls"]: Int ID
+                - a_type = "rect": Int ID
+                - a_type = "poi": String UUID
+                    Example: "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+
+
+
+        Returns:
+            - True => Modify Success
+            - False => Modify Failure
+        """
+        self.__LOGGER.INFO(f"Request: Type:{a_type} | Usage:{a_usage} | ID:{id}")
+        endpoint = ""
+        # Check Input Conditions
+        if a_type == "lines":
+            if a_usage not in ["tracks", "walls"]:
+                self.__LOGGER.WARNING("Invalid Usage Argument Passed")
+                return False
+            elif id is not None:
+                endpoint = f"lines/{a_usage}/{id}"
+            else:
+                endpoint = f"lines/{a_usage}"
+            r_type = Response_Type.JSON
+        elif a_type == "rect":
+            if a_usage not in [
+                "forbidden_area",
+                "elevator_area",
+                "dangerous_area",
+                "coverage_area",
+                "maintenance_area",
+                "sensor_disable_area",
+                "restricted_area",
+            ]:
+                self.__LOGGER.WARNING("Invalid Usage Argument Passed")
+                return False
+            elif id is not None:
+                endpoint = f"rectangle-areas/{a_usage}/{id}"
+            else:
+                endpoint = f"rectangle-areas/{a_usage}"
+            r_type = Response_Type.JSON
+        elif a_type == "poi":
+            a_type = "pois"
+            if id is not None:
+                endpoint = f"pois/{id}"
+                r_type = Response_Type.JSON
+            else:
+                endpoint = "pois"
+                r_type = Response_Type.JSON
+        elif a_type == "laser":
+            endpoint = "laser-landmarks"
+            r_type = Response_Type.JSON
+            a_type = "laser-landmarks"
+        else:
+            self.__LOGGER.WARNING("Invalid Type Argument Passed")
+            return False
+
+        response: combined_Result = self.__REST_ADAPTER.delete(
+            full_endpoint=f"{self.__IP_ADDR}/{self.__API_TAG}/{self.__API_VERSION}/{endpoint}",
+            response_type=r_type,
+        )
+        status_code = response.status_code
+        if status_code == 200:
+            return True
+        else:
+            return False
