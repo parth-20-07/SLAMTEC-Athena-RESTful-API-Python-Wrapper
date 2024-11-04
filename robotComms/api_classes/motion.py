678bac8d979f2455ad90886020ff6d24284eaae1
Parth Patel <parth.pmech@gmail.com>
2024-11-04 13:24:15 -0500

    feat: Add New Motion Function

--- robotComms/api_classes/motion.py
+++ robotComms/api_classes/motion.py
@@ -4,6 +4,7 @@ from robotComms.utils.results import (
     combined_Result,
     Response_Type,
     ListDictType,
+    DictType,
     CombinedType,
 )
 
@@ -47,3 +48,100 @@ class motion:
         )
         result: CombinedType = response.data
         return result
+
+    def get_current_action(
+        self,
+    ) -> DictType:
+        """Get the current behavior
+
+        Returns:
+            {
+                    "action_id": 0,
+                    "action_name": "string",
+                    "stage": "GOING_TO_TARGET",
+                    "state": {
+                        "status": 0,
+                        "result": 0,
+                        "reason": ""
+                        }
+            }
+        """
+        response: combined_Result = self.__REST_ADAPTER.get(
+            full_endpoint=f"{self.__IP_ADDR}/{self.__API_TAG}/{self.__API_VERSION}/actions/:current",
+            response_type=Response_Type.LIST_JSON,
+        )
+        result: CombinedType = response.data
+        return result
+
+    ##############################################################################################################
+    # Setters
+    ##############################################################################################################
+    def delete_current_action(
+        self,
+    ) -> bool:
+        """Terminate the current behavior
+
+        Returns:
+            - True => Action Delete Success
+            - False => Action Delete Failure
+        """
+        response: combined_Result = self.__REST_ADAPTER.delete(
+            full_endpoint=f"{self.__IP_ADDR}/{self.__API_TAG}/{self.__API_VERSION}/actions/:current",
+            response_type=Response_Type.EMPTY,
+        )
+        status_code: int = response.status_code
+        if status_code == 200:
+            return True
+        else:
+            return False
+
+    def create_new_motion(self, request_body: DictType) -> DictType:
+        """Create New Motion Behavior
+
+        Args:
+            request_body: action_name is queried through the `get_supported_actions()` interface, and the specific content of options depends on the action type.
+            Example:
+                {
+                        "action_name": "slamtec.agent.actions.MoveToAction",
+                        "options": {
+                            "target": {
+                                "x": 0,
+                                "y": 0,
+                                "z": 0
+                                },
+                            "move_options": {
+                                "mode": 0,
+                                "flags": [],
+                                "yaw": 0,
+                                "acceptable_precision": 0,
+                                "fail_retry_count": 0,
+                                "speed_ratio": 0
+                                }
+                            }
+                }
+
+        Returns:
+            - Success
+                Example:
+                    {
+                            "action_id": 0,
+                            "action_name": "string",
+                            "stage": "GOING_TO_TARGET",
+                            "state": {
+                                "status": 0,
+                                "result": 0,
+                                "reason": ""
+                                }
+                    }
+            - Failure => Empty Dictionary
+        """
+        response: combined_Result = self.__REST_ADAPTER.post(
+            full_endpoint=f"{self.__IP_ADDR}/{self.__API_TAG}/{self.__API_VERSION}/actions/:current",
+            response_type=Response_Type.JSON,
+            body_params=request_body,
+        )
+
+        if response.status_code == 200:
+            return response.data
+        else:
+            return {}
